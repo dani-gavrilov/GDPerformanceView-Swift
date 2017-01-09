b2746ed3370498021652897ccb3cacd93465f883
Daniil Gavrilov <daniilmbox@gmail.com>
2017-01-10 01:26:51 +0300

    Add Carthage support

---
+++ GDPerformanceViewIOS/GDPerformanceViewIOS.h
@@ -0,0 +1,19 @@
+//
+//  GDPerformanceViewIOS.h
+//  GDPerformanceViewIOS
+//
+//  Created by Daniil Gavrilov on 10.01.17.
+//  Copyright © 2017 Daniil Gavrilov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GDPerformanceViewIOS.
+FOUNDATION_EXPORT double GDPerformanceViewIOSVersionNumber;
+
+//! Project version string for GDPerformanceViewIOS.
+FOUNDATION_EXPORT const unsigned char GDPerformanceViewIOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GDPerformanceViewIOS/PublicHeader.h>
+
+
