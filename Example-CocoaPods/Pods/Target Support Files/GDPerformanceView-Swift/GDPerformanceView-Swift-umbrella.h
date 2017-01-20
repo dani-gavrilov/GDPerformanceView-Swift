851eb0394cde9dbf38c7cc46acae51f67d21536d
Gavrilov Daniil <daniilmbox@gmail.com>
2017-01-20 12:35:28 +0300

    remove examples from repository

--- Example-CocoaPods/Pods/Target Support Files/GDPerformanceView-Swift/GDPerformanceView-Swift-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double GDPerformanceView_SwiftVersionNumber;
-FOUNDATION_EXPORT const unsigned char GDPerformanceView_SwiftVersionString[];
-
