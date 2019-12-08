661f8070aa4cbab8e9255757e06e80ea2877cb92
Daniil Gavrilov <daniilmbox@gmail.com>
2019-12-08 16:31:29 +0300

    modify package.swift

--- Package.swift
+++ Package.swift
@@ -21,7 +21,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "GDPerformanceView-Swift",
-            dependencies: []),
+            dependencies: [],
+            path: "./GDPerformanceView-Swift/GDPerformanceMonitoring"),
         .testTarget(
             name: "GDPerformanceView-SwiftTests",
             dependencies: ["GDPerformanceView-Swift"]),
